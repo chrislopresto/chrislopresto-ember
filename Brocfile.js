048bc889a20da1bf7af73a278b2775f3f5e9b860
Chris LoPresto <chrislopresto@gmail.com>
2014-10-20 02:13:46 -0400

    Carry Font Awesome as bower dependency

--- Brocfile.js
+++ Brocfile.js
@@ -1,8 +1,11 @@
 /* global require, module */
 
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
+var pickFiles = require('broccoli-static-compiler');
 
-var app = new EmberApp();
+var app = new EmberApp({
+  emberCliFontAwesome: { includeFontAwesomeAssets: false }
+});
 
 // Use `app.import` to add additional libraries to the generated
 // output files.
@@ -16,5 +19,18 @@ var app = new EmberApp();
 // modules that you would like to import into your application
 // please specify an object with the list of modules as keys
 // along with the exports of each module as its value.
+app.import('bower_components/font-awesome/css/font-awesome.css');
+var fontAwesome = pickFiles('bower_components/font-awesome/fonts/', {
+  srcDir: '/',
+  // files: isn't strictly necessary (if left out will load all files), but leaving in to be explicit.
+  files: [
+    'fontawesome-webfont.ttf',
+    'fontawesome-webfont.woff',
+    'fontawesome-webfont.eot',
+    'FontAwesome.otf',
+    'fontawesome-webfont.svg'
+  ],
+  destDir: '/fonts'
+});
 
-module.exports = app.toTree();
+module.exports = app.toTree(fontAwesome);
