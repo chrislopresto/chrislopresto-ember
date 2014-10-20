c715922d4781e3db9ad8c1a98aa4f122c0767e8b
Chris LoPresto <chrislopresto@gmail.com>
2014-10-20 02:20:32 -0400

    Add GitHub Pages deploy script

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+git checkout master
+git pull
+rm -rf dist
+
+ember build --environment production
+
+git checkout gh-pages
+git pull
+ls -1 | grep -v -E '^dist|CNAME$' | xargs rm -rf
+mv dist/* ./
+mv dist/.* ./
+rmdir dist
+
+git commit -am "Update"
+git push
+
+git checkout master
+git pull
+./install.sh
---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+rm -rf vendor node_modules tmp
+bower install
+npm install
