70308964a93accaecff604bc34f8bffab5e9241d
Chris LoPresto <chrislopresto@gmail.com>
2015-05-07 04:59:06 -0400

    Simplify markup. Address scss-lint errors.

--- app/components/blog-summary/component.js
+++ app/components/blog-summary/component.js
@@ -3,6 +3,7 @@ import Ember from 'ember';
 const computed = Ember.computed;
 
 export default Ember.Component.extend({
+  classNames: ['blog-summary'],
   showDetails: true,
   hasImage: computed.bool('article.mainImageSrc')
 });
