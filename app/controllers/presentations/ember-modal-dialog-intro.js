69c1c5ca6818f4912669b4ed5e36cc75aba5920d
Chris LoPresto <chris@chrislopresto.com>
2016-01-10 14:40:19 -0500

    Optimize ember-modal-dialog-intro demo

--- app/controllers/presentations/ember-modal-dialog-intro.js
+++ app/controllers/presentations/ember-modal-dialog-intro.js
@@ -1,4 +1,14 @@
+import Ember from 'ember';
 import RevealPresentation from 'ember-reveal-js/controllers/reveal-presentation';
 
+const { computed } = Ember;
+const SLIDES_TO_SHOW_OUTSIDE_WORLD = new Ember.A(['1']);
+
 export default RevealPresentation.extend({
+  outsideWorldClass: computed('h', function() {
+    if (SLIDES_TO_SHOW_OUTSIDE_WORLD.contains(this.get('h'))) {
+      return 'outside-world-show';
+    }
+    return 'outside-world-hide';
+  })
 });
