6f95e4c3b20671b5c6ec70b54cb49e6e0515b42f
Chris LoPresto <chris@chrislopresto.com>
2016-01-16 23:23:55 -0500

    Simplify demo presentation theme switching

--- app/controllers/presentations/demo.js
+++ app/controllers/presentations/demo.js
@@ -1,22 +1,9 @@
-import Ember from 'ember';
 import RevealPresentation from 'ember-reveal-js/controllers/reveal-presentation';
 
-const { set } = Ember;
-
 export default RevealPresentation.extend({
-  presentation: function() {
-    let viewRegistry = this.container.lookup('-view-registry:main');
-
-    // for < 1.12.0 support
-    if (!viewRegistry) {
-      viewRegistry = Ember.View.views;
-    }
-
-    return viewRegistry.demoPresentation;
-  },
   actions: {
     setTheme: function(theme) {
-      set(this.presentation(), 'theme', theme);
+      this.set('t', theme);
     }
   }
 });
