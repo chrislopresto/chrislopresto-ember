8fead08be166e370b3e89c5a58348d59fc2b786b
Chris LoPresto <chris@chrislopresto.com>
2016-02-05 02:28:16 -0500

    Create ember-modal-dialog-intro presentation

--- app/controllers/presentations/ember-modal-dialog-intro.js
+++ app/controllers/presentations/ember-modal-dialog-intro.js
@@ -1,14 +1,16 @@
-import Ember from 'ember';
 import RevealPresentation from 'ember-reveal-js/controllers/reveal-presentation';
 
-const { computed } = Ember;
-const SLIDES_TO_SHOW_OUTSIDE_WORLD = new Ember.A(['1']);
-
 export default RevealPresentation.extend({
-  outsideWorldClass: computed('h', function() {
-    if (SLIDES_TO_SHOW_OUTSIDE_WORLD.contains(this.get('h'))) {
-      return 'the-outside-world-show';
+  actions: {
+    // BEGIN-FREESTYLE-USAGE modal-dialog-controller
+    toggleModalDialog() {
+      this.toggleProperty('showModalDialog');
+    },
+    // END-FREESTYLE-USAGE
+    // BEGIN-FREESTYLE-USAGE tether-dialog-controller
+    toggleTetherDialog() {
+      this.toggleProperty('showTetherDialog');
     }
-    return 'the-outside-world-hide';
-  })
+    // END-FREESTYLE-USAGE
+  }
 });
