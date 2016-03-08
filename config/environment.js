fbbfbc5a638b9f23358d7910509a1ebb7f31818f
Chris LoPresto <chris@chrislopresto.com>
2016-03-08 09:29:28 -0500

    Add living style guide driven development presentation

--- config/environment.js
+++ config/environment.js
@@ -6,6 +6,9 @@ module.exports = function(environment) {
     environment: environment,
     baseURL: '/',
     locationType: 'auto',
+    moment: {
+      includeTimezone: '2010-2020'
+    },
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,11 +5,42 @@ var jsonImporter = require('node-sass-json-importer');
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
     // Add options here
+    nodeAssets: {
+      'cssgram': {
+        srcDir: 'source/scss',
+        import: [
+          '1977.scss',
+          '_shared.scss',
+          'aden.scss',
+          'brooklyn.scss',
+          'clarendon.scss',
+          'cssgram.scss',
+          'earlybird.scss',
+          'gingham.scss',
+          'hudson.scss',
+          'inkwell.scss',
+          'lark.scss',
+          'lofi.scss',
+          'mayfair.scss',
+          'moon.scss',
+          'nashville.scss',
+          'perpetua.scss',
+          'reyes.scss',
+          'rise.scss',
+          'slumber.scss',
+          'toaster.scss',
+          'walden.scss',
+          'willow.scss',
+          'xpro2.scss'
+        ]
+      }
+    },
     emberCliFontAwesome: {
       useScss: true
     },
     fingerprint: {
-      prepend: 'https://s3.amazonaws.com/chrislopresto.com-assets/'
+      prepend: 'https://s3.amazonaws.com/chrislopresto.com-assets/',
+      extensions: ['js', 'css', 'png', 'jpg', 'gif', 'map', 'woff', 'woff2', 'otf', 'eot', 'ttf', 'svg', 'swf', 'ico']
     },
     autoprefixer: {
       browsers: ['last 2 ios version'],
@@ -39,6 +70,10 @@ module.exports = function(defaults) {
 
   app.import('bower_components/highlightjs/styles/tomorrow-night-eighties.css');
   app.import('bower_components/remarkable/dist/remarkable.js');
+
+  app.import('vendor/cssgram/cssgram.scss');
+
+  app.import('vendor/yapp-keen-shim.js');
 
   return app.toTree();
 };
